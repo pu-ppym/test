0c68cf63d6b1689b986d731c06fd4e6bc083fb65
jio <bobbinggi1223@gmail.com>
2024-11-13 10:56:14 +0900

    prime number v0.2

--- week12.py
+++ week12.py
@@ -1,12 +1,19 @@
-counts = 0
+# counts = 0
+is_prime = True
 number = int(input("Input number : "))
 
-for i in range(1, number+1):
-    if number % i == 0:
-        counts = counts + 1
-    print(i, end=' ')
+if number <= 1:
+    is_prime = False
+else:
+    for i in range(2, number):
+        if number % i == 0:
+            # counts = counts + 1
+            is_prime = False
+        print(i, end=' ')
 
-if counts == 2:
+
+# if counts == 2:
+if is_prime:
     print(f"\n{number} is prime number~")
 else:
-    print(f"\n{number} is NOT prime number! (divisors : {counts})")
+    print(f"\n{number} is NOT prime number!")
